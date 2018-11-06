560b672d36e79d69a97ba5720ef03a95562f155f
Jan Huber - M16721 <jan.huber@k2l.de>
2018-11-06 09:49:00 +0100

    Get new topic name from user
    Create a small window where the user can enter the new topic name.
    Topic name can be confirmed by hitting RETURN.
    The new topic name will then be written to the topics file

--- topics.py
+++ topics.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # vim: ts=4 sw=4 et ft=py
 
+import curses
 import os
+
 
 class Topics:
     filepath = ""
@@ -9,6 +11,7 @@ class Topics:
     topic_names = []
     is_menu_active = False
     menu_pos = 0
+
 
     def __init__(self):
         # get full filepath
@@ -22,9 +25,23 @@ class Topics:
         # get num of topics
         self.num_topics = len(self.topic_names)
 
-    def new_topic(self):
-        new_t = input("Topic name: ")
 
+    def new_topic(self, height, width):
+        new_t = ""
+
+        twin = curses.newwin(3, 50, (height // 2), (width // 2) - 25)
+        twin.border()
+        curses.echo()
+        y, x = twin.getyx()
+        twin.addstr(y + 1, x + 1, "New topic: ")
+        twin.refresh()
+
+        # getstr() returns a byte object rather than a string
+        # this means, it must be decoded first
+        # python3 problem only
+        new_t = twin.getstr().decode(encoding="utf-8") + '\n'
+
+        # write new topic to file
         f = open(self.filepath, "a")
         f.write(new_t)
         f.close()
